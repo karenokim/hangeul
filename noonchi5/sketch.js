61505deab6d7a89f8886a517cc515eb08ddd6285
karenokim <gkim11@risd.edu>
2023-02-03 14:06:12 -0500

    fin

--- noonchi5/sketch.js
+++ noonchi5/sketch.js
@@ -8,8 +8,8 @@ function preload() {
 }
 
 function setup() { 
-  createCanvas(windowWidth, windowHeight);
-  pts2 = needle.textToPoints('썩    소', windowWidth/2, windowHeight/2, 250,
+  createCanvas(600, 400);
+  pts2 = needle.textToPoints('썩    소', 0, 0, 250,
                           {sampleFactor: 0.9,
                            simplifyThreshold: 0});
 } 
@@ -25,17 +25,15 @@ function ns(x, y, z, scale_, min_, max_) {
 function draw() { 
   background(0);
   noStroke();
-  textFont(caslon);
   fill(255);
-  text("썩소 is a shortened word with two words combined: 썩은, meaning rotten, and 미소, meaning a smile. Combined together, this word refers to a smile that is sarcastic or empty. ",windowWidth/2,900,400,400);
- 
-  
+  push();
+  translate(75, 275);
   for (let i = 0; i < pts2.length; i++) {
     let xoff = ns(pts2[i].x, pts2[i].y, xz, 0.005, -50, 50);
     let yoff = ns(pts2[i].y, pts2[i].x, yz, 0.005, -50, 50);
     ellipse(pts2[i].x + xoff, pts2[i].y + yoff, 5, 5);
   }
-
+  pop();
   xz += 2;
   yz += 2;
 }
