6e0ad034a984196372abd99840786c52de42a7f2
karenokim <gkim11@risd.edu>
2023-02-03 13:23:40 -0500

    new

--- noonchi3/sketch.js
+++ noonchi3/sketch.js
@@ -1,7 +1,7 @@
 let pts;
 let needle;
 let caslon;
-var t;
+// var t;
 
 function preload(){
   needle = loadFont('Ghanachocolate.otf');
@@ -12,28 +12,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(16);
   textAlign(CENTER,CENTER);
-    t = 0;
+    // t = 0;
 
-  pts = needle.textToPoints('부들부들', windowWidth/2, windowHeight/2, 140,{
+  pts = needle.textToPoints('부들부들', windowWidth/2-200, windowHeight/2, 140,{
     sampleFactor: 0.3,
     simplifyThreshold: 0
   });
 }
 
 function draw() {
-   var x = width * noise(t);
-  var y = height * noise(t+5);
-  var r = 255 * noise(t+40)+100;
-  var g = 255 * noise(t+15)+100;
-  var b = 255 * noise(t+20)+100;
+  //  var x = width * noise(t);
+  // var y = height * noise(t+5);
+  // var r = 255 * noise(t+40)+100;
+  // var g = 255 * noise(t+15)+100;
+  // var b = 255 * noise(t+20)+100;
   textFont(caslon);
 
   background(0,0,0);
     fill(255);
   text("부들부들 is an onomatopoeia used when caught in a nerve wracking situation",windowWidth/2,900,400,400);
   
-  stroke(r,g,b);
-    t += 0.01;
+  stroke(255);
+    // t += 0.01;
   for(let i =0; i< pts.length; i=i+2){
    line(pts[i].x+10, pts[i].y+10, pts[i].x*1 + random(-15,15), pts[i].y + random(10,15));
     
---
+++ noonchi6/sketch.js
@@ -0,0 +1,105 @@
+let needle;
+let pts1;
+let pts2;
+let pts3;
+let pts4;
+let pts5;
+
+function preload() {
+  needle = loadFont("Ghanachocolate.otf");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  strokeWeight(1);
+  pts1 = needle.textToPoints("불난 집에 부채질", 32, 140, 140, {
+    sampleFactor: 0.17,
+  });
+  pts2 = needle.textToPoints("썩소", 32, 330, 150, {
+    sampleFactor: 0.6524,
+  });
+  pts3 = needle.textToPoints("하늘의 별 따기", -20, 160, 150, {
+    sampleFactor: 0.1984,
+  });
+  pts4 = needle.textToPoints("아리송", 350, 330, 150, {
+    sampleFactor: 0.4354,
+  });
+  pts5 = needle.textToPoints("부들부들", 820, 330, 150, {
+    sampleFactor: 0.2757,
+  });
+  
+  print(pts1.length,pts2.length,pts3.length,pts4.length,pts5.length)
+}
+
+function draw() {
+  background(0, 0, 0, 10);
+
+  for (let i = 0; i < pts1.length; i = i + 2) {
+    stroke(180, 200, 200, 100);
+    line(
+      pts1[i].x + 10,
+      pts1[i].y + 10,
+      pts1[i].x * 1 + random(-15, 15),
+      pts1[i].y + random(10, 15)
+    );
+    noFill();
+  }
+
+  for (let i = 0; i < pts2.length; i++) {
+    fill(180, 200, 200, 100);
+    let xoff = ns(pts2[i].x, pts2[i].y, xz, 0.005, -50, 50);
+    let yoff = ns(pts2[i].y, pts2[i].x, yz, 0.005, -50, 50);
+    ellipse(pts2[i].x + xoff, pts2[i].y + yoff, 5, 5);
+  }
+  xz += 3;
+  yz += 3;
+
+  const d = sin(frameCount / 50) * 2;
+  const angle = frameCount / 100;
+  push();
+  translate(60, height / 2);
+  for (let i = 0; i < pts3.length; i++) {
+    push();
+    translate(pts3[i].x, pts3[i].y);
+    rotate(angle);
+    line(-d, -d, +d, +d);
+    line(+d, -d, -d, +d);
+    pop();
+  }
+  pop();
+
+  //아리송
+  beginShape(POINTS);
+  // stroke(255);
+  for (let i = 0; i < pts4.length; i++) {
+      push()
+        translate(pts4[i].x.mouseX,pts4[i].y.mouseY);
+
+    vertex(
+      pts4[i].x + sin(frameCount * 0.05 + pts4[i].y * 0.1) * 10,
+      pts4[i].y );
+    
+    pop()
+   
+  }
+  endShape();
+  
+
+
+for (let i = 0; i < pts5.length; i++) {
+  let xoff = ns(pts5[i].x, pts5[i].y, xz, 0.005, -80, 80);
+  let yoff = ns(pts5[i].y, pts5[i].x, yz, 0.005, -80, 80);
+  rect(pts5[i].x + xoff, pts5[i].y + yoff, 2, 2);
+}
+
+xz += 15;
+yz += 5;
+}
+
+function ns(x, y, z, scale_, min_, max_) {
+  return map(noise(x * scale_, y * scale_, z * scale_), 0, 1, min_, max_);
+}
+
+let xz = 0;
+let yz = 1000;
